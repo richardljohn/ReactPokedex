5c65f8296a25660364f463237a5fc94ac0b6dd8c
richardljohn <richardljohn@gmail.com>
2021-04-12 22:49:02 -0400

    Got a bit more done. Still starting out

--- my-pokedex/src/App.js
+++ my-pokedex/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState } from 'react';
+import PokemonList from './components/PokemonList';
 
 function App() {
+  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
   return (
     null
   );
