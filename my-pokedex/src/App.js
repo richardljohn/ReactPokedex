21ef5f991ff98f89e57b91938dfe664a1decf989
richardljohn <richardljohn@gmail.com>
2023-01-20 13:16:03 -0500

    Reconstructing cont.

--- my-pokedex/src/App.js
+++ my-pokedex/src/App.js
@@ -1,66 +1,62 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
 import PokemonList from './components/PokemonList';
-import Pagination from './components/Pagination';
-import Pokemon from './components/Pokemon';
-import axios from 'axios';
 
 function App() {
 
-  const [pokemon, setPokemon] = useState([])
-  const [selectedPokemon, setSelectedPokemon] = useState()
-  const [currPage, setCurrPage] = useState("https://pokeapi.co/api/v2/pokemon")
-  const [prevPage, setPrevPage] = useState()
-  const [nextPage, setNextPage] = useState()
-  const [loading, setLoading] = useState(true)
+  // const [pokemon, setPokemon] = useState([])
+  // const [selectedPokemon, setSelectedPokemon] = useState()
+  // const [currPage, setCurrPage] = useState("https://pokeapi.co/api/v2/pokemon")
+  // const [prevPage, setPrevPage] = useState()
+  // const [nextPage, setNextPage] = useState()
+  // const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    setLoading(true)
-    let cancel
-    axios.get(currPage, {
-      cancelToken: new axios.CancelToken(c => cancel = c)
-    }).then((res) => {
-            setLoading(false)
-            setNextPage(res.data.next)
-            //console.log(nextPage)
-            setPrevPage(res.data.previous)
-            //console.log(prevPage)
-            setPokemon(res.data.results.map((p) => p.name))
-    })
+  // useEffect(() => {
+  //   setLoading(true)
+  //   let cancel
+  //   axios.get(currPage, {
+  //     cancelToken: new axios.CancelToken(c => cancel = c)
+  //   }).then((res) => {
+  //           setLoading(false)
+  //           setNextPage(res.data.next)
+  //           //console.log(nextPage)
+  //           setPrevPage(res.data.previous)
+  //           //console.log(prevPage)
+  //           setPokemon(res.data.results.map((p) => p.name))
+  //   })
 
-    return () => cancel()
+  //   return () => cancel()
       
-  }, [currPage])
+  // }, [currPage])
 
-  function goToNextPage(){ 
-    setCurrPage(nextPage)
-  }
+  // function goToNextPage(){ 
+  //   setCurrPage(nextPage)
+  // }
 
-  function goToPrevPage() { 
-    setCurrPage(prevPage)
-  }
+  // function goToPrevPage() { 
+  //   setCurrPage(prevPage)
+  // }
 
-  if(loading){
-    return "Loading...."
-  } 
+  // if(loading){
+  //   return "Loading...."
+  // } 
 
-  function getPokemonByName(name){
-    setSelectedPokemon(name)       
-  }
+  // function getPokemonByName(name){
+  //   setSelectedPokemon(name)       
+  // }
 
-  if(selectedPokemon) { 
-    return ( 
-      <div>
-        <p><Pokemon selectedPokemon={selectedPokemon}/></p>
-      </div>
-    )
-  }
+  // if(selectedPokemon) { 
+  //   return ( 
+  //     <div>
+  //       <p><Pokemon selectedPokemon={selectedPokemon}/></p>
+  //     </div>
+  //   )
+  // }
 
   return (
-    <>
-      <PokemonList pokemon={pokemon}/>
-      <Pagination goToNextPage={nextPage ? goToNextPage : null} goToPrevPage={prevPage ? goToPrevPage : null}/>
-    </>
+    <div className="App">
+      <PokemonList />
+    </div>
   );
 }
 export default App;
--- my-pokedex/src/components/PokemonList.js
+++ my-pokedex/src/components/PokemonList.js
@@ -1,19 +1,55 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Pagination from './Pagination';
 
-function PokemonList({pokemon}) { 
+function PokemonList() { 
+
+    const [pokemon, setPokemon] = useState([])
+    const [selectedPokemon, setSelctedPokemon] = useState()
+    const [currPage, setCurrPage] = useState("https://pokeapi.co/api/v2/pokemon")
+    const [prevPage, setPrevPage] = useState()
+    const [nextPage, setNextPage] = useState()
+    const [loading, setLoading] = useState(true)
+    
+    useEffect(() => {
+        setLoading(true)
+        let cancel
+        axios.get(currPage, {
+          cancelToken: new axios.CancelToken(c => cancel = c)
+        }).then((res) => {
+                setLoading(false)
+                setNextPage(res.data.next)
+                //console.log(nextPage)
+                setPrevPage(res.data.previous)
+                //console.log(prevPage)
+                setPokemon(res.data.results.map((p) => p.name))
+        })
+    
+        return () => cancel()
+          
+      }, [currPage])
+
+    function goToNextPage(){ 
+    setCurrPage(nextPage)
+    }
+
+    function goToPrevPage() { 
+    setCurrPage(prevPage)
+    }
 
     function getPokemonByName(name){ 
-        return ""
+        setSelctedPokemon(name)
     }
 
     return (
-        <div>
-            {pokemon.map((p) => ( 
-                <p key={p}><button class="pokemon-btn btn-primary" onClick={() => getPokemonByName(p.name)}>{p.charAt(0).toUpperCase() + p.slice(1)}</button></p>
-                //<div key={p}>{p.charAt(0).toUpperCase() + p.slice(1)}</div>
-            ))}
-        </div>
+        <>
+            <div>
+                {pokemon.map((p) => ( 
+                    <p key={p}><button class="pokemon-btn btn-primary" onClick={() => getPokemonByName(p.name)}>{p.charAt(0).toUpperCase() + p.slice(1)}</button></p>
+                ))}
+            </div>
+            <Pagination goToNextPage={nextPage ? goToNextPage : null} goToPrevPage={prevPage ? goToPrevPage : null}/>
+        </>
     )
 }
 
